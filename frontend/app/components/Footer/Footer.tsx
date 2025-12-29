12f2707415d28defaa562aba6fceccc0fbfaa40b
NextKolya <phelixgluy@gmail.com>
2025-12-30 01:17:37 +0600

    Add Footer component

---
+++ frontend/app/components/Footer/Footer.tsx
@@ -0,0 +1,5 @@
+import footerStyles from './Footer.module.scss';
+
+export default function Footer() {
+    return <footer className={footerStyles['footer']}> </footer>;
+}
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
 import Header from './components/Header';
+import Footer from './components/Footer';
 import SvgSprites from './components/SvgSprites';
 
 import './globals.scss';
@@ -37,6 +38,7 @@ export default function RootLayout({
                 <SvgSprites />
 
                 {children}
+                <Footer />
             </body>
         </html>
     );
