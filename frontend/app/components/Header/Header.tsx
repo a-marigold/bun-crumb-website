c8a9094f4246961406f063098eef7adf741181a6
NextKolya <phelixgluy@gmail.com>
2025-12-29 21:35:50 +0600

    Add NavPanel in Header

--- frontend/app/components/Header/Header.tsx
+++ frontend/app/components/Header/Header.tsx
@@ -1,19 +1,33 @@
 import Link from 'next/link';
 
+import NavLink from '@/UI/NavLink/NavLink';
+import type { NavLinkProps } from '@/UI/NavLink/NavLink';
+
 import headerStyles from './Header.module.scss';
+
+const navLinkList: NavLinkProps[] = [
+    { href: '/about', title: 'About', 'aria-label': 'Go to "about" page' },
+
+    { href: '/docs', title: 'Docs', 'aria-label': 'Go to documentation page' },
+];
 
 export default function Header() {
     return (
-        <header>
+        <header className={headerStyles['header']}>
             <Link
                 href='/'
                 prefetch={false}
                 className={headerStyles['logo-link']}
             >
-                Crumb
+                <svg width={67} height={16} color='var(--font-color)'>
+                    <use href='#crumb-logo' />
+                </svg>
             </Link>
-
-            <nav className={headerStyles['nav-panel']}> </nav>
+            <nav className={headerStyles['nav-panel']}>
+                {navLinkList.map((props) => (
+                    <NavLink {...props} />
+                ))}
+            </nav>
         </header>
     );
 }
