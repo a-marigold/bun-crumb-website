5d910128d9f355a896e6702a4e4a76914278ece6
NextKolya <phelixgluy@gmail.com>
2025-12-29 20:58:42 +0600

    Add NavLink UI component

---
+++ frontend/UI/NavLink/NavLink.tsx
@@ -0,0 +1,26 @@
+import Link, { type LinkProps } from 'next/link';
+
+import linkStyles from './NavLink.module.scss';
+
+export interface NavLinkProps extends LinkProps {
+    title: string;
+    'aria-label': string;
+
+    className?: string;
+}
+
+export default function NavLink({
+    title,
+    className,
+    ...attributes
+}: NavLinkProps) {
+    return (
+        <Link
+            {...attributes}
+            className={`${linkStyles['nav-link']} ${className ?? ''}`}
+        >
+            {' '}
+            {title}{' '}
+        </Link>
+    );
+}
