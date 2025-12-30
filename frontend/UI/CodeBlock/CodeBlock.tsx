eb8ac20de5c03f4a80759a19c20d55841cefad5a
NextKolya <phelixgluy@gmail.com>
2025-12-30 18:52:57 +0600

    Add CodeBlock UI component

---
+++ frontend/UI/CodeBlock/CodeBlock.tsx
@@ -0,0 +1,18 @@
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { stackoverflowDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import codeStyles from './CodeBlock.module.scss';
+
+export interface CodeBlockProps {
+    codeString: string;
+}
+export default function CodeBlock({ codeString }: CodeBlockProps) {
+    return (
+        <SyntaxHighlighter
+            language='javascript'
+            style={stackoverflowDark}
+            showLineNumbers
+        >
+            {codeString}
+        </SyntaxHighlighter>
+    );
+}
