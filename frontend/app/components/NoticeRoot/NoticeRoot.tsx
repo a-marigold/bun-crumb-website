6f4851d0f45a5dc1ed7aaea258a0d5c73293a733
NextKolya <phelixgluy@gmail.com>
2026-01-01 23:36:48 +0600

    Add NoticeRoot component

---
+++ frontend/app/components/NoticeRoot/NoticeRoot.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+// g n r
+
+import { useEffect } from 'react';
+
+import { useNoticeStore } from '@/store/NoticeStore';
+
+import Notice from '@/UI/Notice';
+
+import noticeStyles from './NoticeRoot.module.scss';
+
+export default function NoticeRoot() {
+    const currentNotice = useNoticeStore((state) => state.currentNotice);
+    const currentExistenceTime = useNoticeStore(
+        (state) => state.currentExistenceTime
+    );
+
+    useEffect(() => {});
+
+    return (
+        <div className={noticeStyles['notice-root']}>
+            {currentNotice && <Notice {...currentNotice} />}
+        </div>
+    );
+}
