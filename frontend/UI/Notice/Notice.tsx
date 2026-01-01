48c786aa9ea0788a6d778c43d412426236a2df80
NextKolya <phelixgluy@gmail.com>
2026-01-01 23:29:15 +0600

    Add Notice UI component

---
+++ frontend/UI/Notice/Notice.tsx
@@ -0,0 +1,25 @@
+import noticeStyles from './Notice.module.scss';
+
+export interface NoticeProps {
+    title: string;
+
+    onClose: () => void;
+}
+export default function Notice({ title, onClose }: NoticeProps) {
+    return (
+        <div role='status' className={noticeStyles['notice']}>
+            <div className={noticeStyles['text-block']}>
+                <span className={noticeStyles['title']}>{title}</span>
+            </div>
+
+            <button className={noticeStyles['close-button']} onClick={onClose}>
+                Close
+            </button>
+        </div>
+    );
+}
+
+// !
+
+type a = 's' | 'b' | (string & {});
+// !
---
+++ frontend/UI/Notice/index.ts
@@ -0,0 +1,1 @@
+export { default, type NoticeProps } from './Notice';
